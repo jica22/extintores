8f358f92328f18519396d97b2694facabd4a33bd
jica22 <joaobrsgp@hotmail.com>
2025-06-04 21:46:35 -0300

    Set default admin value.

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ $senha = $data->senha;
 // Criptografar senha
 $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+$sql = "INSERT INTO usuarios (nome, email, senha, admin) VALUES (?, ?, ?, false)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("sss", $nome, $email, $senha_hash);
 
